74e8930cac171effb9ae943a3023e4bcfecfda27
破竹 <poozhu@qq.com>
2024-05-15 23:11:51 +0800

    feat: react 组件新增 module less 选项

--- src/extension.ts
+++ src/extension.ts
@@ -4,47 +4,63 @@ const Mustache = require("mustache");
 
 import { tsxStr, lessStr } from "./template";
 
+function createComponent(path: string, lessType?: "default" | "module") {
+  vscode.window.showInputBox().then((componentName) => {
+    if (!componentName) {
+      return;
+    }
+    if (!/^([A-Z])/.test(componentName)) {
+      vscode.window.showErrorMessage("组件名请用大写开头！");
+      return;
+    }
+    const className = componentName.replace(/^([A-Z])/, (match) => {
+      return match.toLocaleLowerCase();
+    });
+
+    const tsxString = Mustache.render(tsxStr, { componentName, className });
+    const lessString = Mustache.render(lessStr, { className });
+    fs.mkdirSync(`${path}\\${componentName}`, () => {});
+    fs.writeFileSync(
+      `${path}\\${componentName}\\index.tsx`,
+      tsxString,
+      (err: any) => {
+        vscode.window.showErrorMessage(err);
+      }
+    );
+    fs.writeFileSync(
+      `${path}\\${componentName}\\index${
+        lessType === "module" ? ".module" : ""
+      }.less`,
+      lessString,
+      (err: any) => {
+        vscode.window.showErrorMessage(err);
+      }
+    );
+    vscode.window.showInformationMessage(`${componentName} 组件新建成功！`);
+  });
+}
+
 export function activate(context: vscode.ExtensionContext) {
   console.log("简单编码已加载！");
 
   const createReactComponent = vscode.commands.registerCommand(
     "easy-coding.createReactComponent",
     (res) => {
-      vscode.window.showInputBox().then((componentName) => {
-        if (!componentName) {
-          return;
-        }
-        if (!/^([A-Z])/.test(componentName)) {
-          vscode.window.showErrorMessage("组件名请用大写开头！");
-          return;
-        }
-        const className = componentName.replace(/^([A-Z])/, (match) => {
-          return match.toLocaleLowerCase();
-        });
-
-        const tsxString = Mustache.render(tsxStr, { componentName, className });
-        const lessString = Mustache.render(lessStr, { className });
-        fs.mkdirSync(`${res.fsPath}\\${componentName}`, () => {});
-        fs.writeFileSync(
-          `${res.fsPath}\\${componentName}\\index.tsx`,
-          tsxString,
-          (err: any) => {
-            vscode.window.showErrorMessage(err);
-          }
-        );
-        fs.writeFileSync(
-          `${res.fsPath}\\${componentName}\\index.less`,
-          lessString,
-          (err: any) => {
-            vscode.window.showErrorMessage(err);
-          }
-        );
-        vscode.window.showInformationMessage(`${componentName} 组件新建成功！`);
-      });
+      createComponent(res.fsPath);
     }
   );
 
-  context.subscriptions.push(createReactComponent);
+  const createReactComponentWithModuleLess = vscode.commands.registerCommand(
+    "easy-coding.createReactComponentWithModuleLess",
+    (res) => {
+      createComponent(res.fsPath, "module");
+    }
+  );
+
+  context.subscriptions.push(
+    createReactComponent,
+    createReactComponentWithModuleLess
+  );
 }
 
 export function deactivate() {}
